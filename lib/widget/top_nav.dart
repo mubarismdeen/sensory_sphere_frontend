6dcfc29f9ebc75b4f6f9ce6a0138680d36b572f6
mubarismdeen <mubarisv@gmail.com>
2024-02-26 23:44:59 +0530

    fixing windows build issue

--- lib/widget/top_nav.dart
+++ lib/widget/top_nav.dart
@@ -5,6 +5,7 @@ import 'package:admin/widget/settings_popup.dart';
 import 'package:flutter/material.dart';
 
 import '../globalState.dart';
+import '../helpers/image_placeholder.dart';
 import 'custom_alert_dialog.dart';
 import 'custom_text.dart';
 
@@ -19,13 +20,10 @@ AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
               child: Container(
                 height: 35,
                 padding: const EdgeInsets.only(left: 10, top: 5),
-                child: ExcludeSemantics(
-                  child: ColorFiltered(
-                    colorFilter: const ColorFilter.mode(
-                      mainTitleColor, // Desired color
-                      BlendMode.srcIn,
-                    ),
-                    child: Image.asset('images/app_icon.png'),
+                child: const ExcludeSemantics(
+                  child: FadeInImagePlaceholder(
+                    image: AssetImage('images/app_icon.png'),
+                    placeholder: SizedBox.shrink(),
                   ),
                 ),
               ),
@@ -35,88 +33,86 @@ AppBar topNavigationBar(BuildContext context, GlobalKey<ScaffoldState> key) =>
               onPressed: () {
                 key.currentState?.openDrawer();
               }),
-      title: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-          if (!ResponsiveWidget.isSmallScreen(context))
-            const Padding(
-              padding: EdgeInsets.only(top: 10),
-              child: Flexible(
+      title: Wrap(children: [
+        Row(
+          children: [
+            if (!ResponsiveWidget.isSmallScreen(context))
+              const Visibility(
                 child: Text(
                   appTitle,
                   style: TextStyle(
                       color: mainTitleColor,
-                      fontSize: 22,
+                      fontSize: 20,
                       fontWeight: FontWeight.bold,
                       fontFamily: 'LobsterFont'),
                 ),
               ),
+            Expanded(child: Container()),
+            IconButton(
+              icon: Icon(
+                Icons.settings,
+                color: lightGrey.withOpacity(.7),
+              ),
+              onPressed: () {
+                _openSettings(context);
+              },
             ),
-          Expanded(child: Container()),
-          IconButton(
-            icon: Icon(
-              Icons.settings,
-              color: lightGrey.withOpacity(.7),
+            Stack(
+              children: [
+                IconButton(
+                    icon: Icon(
+                      Icons.notifications,
+                      color: lightGrey.withOpacity(.7),
+                    ),
+                    onPressed: () {}),
+                Positioned(
+                    top: 7,
+                    right: 7,
+                    child: Container(
+                      width: 8,
+                      height: 12,
+                      padding: const EdgeInsets.all(4),
+                      decoration: BoxDecoration(
+                          color: active,
+                          borderRadius: BorderRadius.circular(30),
+                          border: Border.all(color: themeColor, width: 2)),
+                    ))
+              ],
             ),
-            onPressed: () {
-              _openSettings(context);
-            },
-          ),
-          Stack(
-            children: [
-              IconButton(
-                  icon: Icon(
-                    Icons.notifications,
-                    color: lightGrey.withOpacity(.7),
-                  ),
-                  onPressed: () {}),
-              Positioned(
-                  top: 7,
-                  right: 7,
-                  child: Container(
-                    width: 8,
-                    height: 12,
-                    padding: const EdgeInsets.all(4),
-                    decoration: BoxDecoration(
-                        color: active,
-                        borderRadius: BorderRadius.circular(30),
-                        border: Border.all(color: themeColor, width: 2)),
-                  ))
-            ],
-          ),
-          Container(
-            width: 1,
-            height: 22,
-            color: lightGrey,
-          ),
-          const SizedBox(
-            width: 5,
-          ),
-          CustomText(
-              text: GlobalState.username,
-              size: 16,
+            Container(
+              width: 1,
+              height: 22,
               color: lightGrey,
-              weight: FontWeight.bold),
-          const SizedBox(
-            width: 5,
-          ),
-          Container(
-            decoration: BoxDecoration(
-                color: lightGrey, borderRadius: BorderRadius.circular(30)),
-            child: Container(
-              padding: const EdgeInsets.all(2),
-              margin: const EdgeInsets.all(2),
-              child: const CircleAvatar(
-                backgroundColor: themeColor,
-                child: Icon(
-                  Icons.person_outline,
-                  color: lightGrey,
+            ),
+            const SizedBox(
+              width: 5,
+            ),
+            CustomText(
+                text: GlobalState.username,
+                size: 16,
+                color: lightGrey,
+                weight: FontWeight.bold),
+            const SizedBox(
+              width: 5,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                  color: lightGrey, borderRadius: BorderRadius.circular(30)),
+              child: Container(
+                padding: const EdgeInsets.all(2),
+                margin: const EdgeInsets.all(2),
+                child: const CircleAvatar(
+                  backgroundColor: themeColor,
+                  child: Icon(
+                    Icons.person_outline,
+                    color: lightGrey,
+                  ),
                 ),
               ),
-            ),
-          )
-        ],
-      ),
+            )
+          ],
+        ),
+      ]),
       iconTheme: const IconThemeData(color: lightGrey),
     );
 
